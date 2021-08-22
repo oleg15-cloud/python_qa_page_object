01204b6c6de031d002bb3db970b79c2474a8acb7
oleg15-cloud <karaivanoleg093@gmail.com>
2021-08-22 23:10:52 +0300

    Add test 'delete_product_in_the_admin_section'

--- tests/test_opencart_admin.py
+++ tests/test_opencart_admin.py
@@ -8,4 +8,14 @@ def test_adding_a_new_product_in_the_admin_section(browser, user):
     MainPageAdmin(browser).go_to_product_page()
     ProductPageAdmin(browser).go_to_add_new_product_form()
     product_name = ProductPageAdmin(browser).create_new_product()
-    ProductPageAdmin(browser).check_new_product_in_product_list(product_name)
+    ProductPageAdmin(browser).check_product_in_product_list(product_name)
+
+
+def test_delete_product_in_the_admin_section(browser, user):
+    LoginPageAdmin(browser).authorization_with(*user)
+    MainPageAdmin(browser).go_to_product_page()
+    ProductPageAdmin(browser)\
+        .select_product_in_product_list_by_position(20)\
+        .delete_product_in_product_list()\
+        .check_alert_message()
+
