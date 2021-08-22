b8c867d42c6898ecbf964dac88ec6b5407b829b4
oleg15-cloud <karaivanoleg093@gmail.com>
2021-08-22 23:09:20 +0300

    Add select, delete func

--- page_object/opencart_admin/ProductPageAdmin.py
+++ page_object/opencart_admin/ProductPageAdmin.py
@@ -5,6 +5,7 @@ import time
 
 class ProductPageAdmin:
     BTN_ADD_NEW_PRODUCT = (By.XPATH, "//i[@class='fa fa-plus']")
+    BTN_DELETE_PRODUCT = (By.XPATH, "//button[@data-original-title='Delete']")
     BTN_SAVE_PRODUCT = (By.XPATH, "//i[@class='fa fa-save']")
     BTN_FILTER = (By.XPATH, "//button[@id='button-filter']")
 
@@ -13,7 +14,12 @@ class ProductPageAdmin:
     INPUT_MODEL = (By.XPATH, "//input[@id='input-model']")
     INPUT_PRODUCT_NAME_FILTER = (By.XPATH, "//input[@name='filter_name']")
 
+    TABLE_ELEMENTS = (By.XPATH, "//tbody/tr")
+    CHECKBOXES = (By.XPATH, "//input[@type='checkbox']")
+
     DATA_SECTION = (By.XPATH, "//a[text()='Data']")
+
+    ALERT_MESSAGE = (By.XPATH, "//div[@class='alert alert-success alert-dismissible']")
 
     def __init__(self, browser):
         self.browser = browser
@@ -33,8 +39,21 @@ class ProductPageAdmin:
         self.browser.find_element(*self.BTN_SAVE_PRODUCT).click()
         return product_name
 
-    def check_new_product_in_product_list(self, product_name):
+    def check_product_in_product_list(self, product_name):
         self.browser.find_element(*self.INPUT_PRODUCT_NAME_FILTER).clear()
         self.browser.find_element(*self.INPUT_PRODUCT_NAME_FILTER).send_keys(product_name)
         self.browser.find_element(*self.BTN_FILTER).click()
         self.browser.find_element(By.XPATH, f"//td[text()='{product_name}']")
+
+    def select_product_in_product_list_by_position(self, position):
+        self.browser.find_elements(*self.CHECKBOXES)[position].click()
+        return self
+
+    def delete_product_in_product_list(self):
+        self.browser.find_element(*self.BTN_DELETE_PRODUCT).click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
+        return self
+
+    def check_alert_message(self):
+        self.browser.find_element(*self.ALERT_MESSAGE)
