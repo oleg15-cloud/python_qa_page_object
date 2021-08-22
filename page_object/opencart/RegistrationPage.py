7c005140644d1c433546357a77a7b0131a606cf5
oleg15-cloud <karaivanoleg093@gmail.com>
2021-08-23 01:02:49 +0300

    Created class RegistrationPage

---
+++ page_object/opencart/RegistrationPage.py
@@ -0,0 +1,36 @@
+import random
+import time
+from selenium.webdriver.common.by import By
+
+
+class RegistrationPage:
+    INPUT_FIRST_NAME = (By.XPATH, "//input[@name='firstname']")
+    INPUT_LAST_NAME = (By.XPATH, "//input[@name='lastname']")
+    INPUT_EMAIL = (By.XPATH, "//input[@name='email']")
+    INPUT_PHONE = (By.XPATH, "//input[@name='telephone']")
+    INPUT_PASSWORD = (By.XPATH, "//input[@name='password']")
+    INPUT_PASSWORD_CONFIRM = (By.XPATH, "//input[@name='confirm']")
+
+    CHECKBOX_PRIVACY_POLICY = (By.XPATH, "//input[@name='agree']")
+
+    BTN_CONTINUE = (By.XPATH, "//input[@value='Continue']")
+
+    SUCCESS_REGISTRATION_MESSAGE = (By.XPATH, "//h1[text()='Your Account Has Been Created!']")
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def create_new_user(self):
+        _time = time.time_ns()
+        self.browser.find_element(*self.INPUT_FIRST_NAME).send_keys(f"user{_time}")
+        self.browser.find_element(*self.INPUT_LAST_NAME).send_keys(f"user{_time}")
+        self.browser.find_element(*self.INPUT_EMAIL).send_keys(f"user{_time}@test.com")
+        self.browser.find_element(*self.INPUT_PHONE).send_keys(f"{_time}")
+        self.browser.find_element(*self.INPUT_PASSWORD).send_keys(f"qwerty")
+        self.browser.find_element(*self.INPUT_PASSWORD_CONFIRM).send_keys(f"qwerty")
+        self.browser.find_element(*self.CHECKBOX_PRIVACY_POLICY).click()
+        self.browser.find_element(*self.BTN_CONTINUE).click()
+        return self
+
+    def check_success_registration_message(self):
+        self.browser.find_element(*self.SUCCESS_REGISTRATION_MESSAGE)
